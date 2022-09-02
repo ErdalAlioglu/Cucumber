a0e60e04f87cd8541d10427954aeade94b930a9f
erdalalioglu <erdalalioglu@gmail.com>
2022-09-03 00:22:43 +0200

    CucumberTest

---
+++ src/test/java/AppiumTest/runner/runners.java
@@ -0,0 +1,22 @@
+package AppiumTest.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={"html:target\\cucumber-reportsApi.html",
+                "json:target/json-reports/Pcucumber1.json",
+                "junit:target/xml-report/Pcucumber1.xml"},
+        features = "src/test/resources/features",
+        glue = "AppiumTest/stepdefination",
+        tags = "@wifi",
+        dryRun = false
+)
+
+
+public class runners {
+
+}
